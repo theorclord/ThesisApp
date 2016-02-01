93cdc6c891a1904b7fae45a578c360c56946ad6f
theorclord <mikkeltheorclord@hotmail.com>
2016-02-01 17:29:46 +0100

    Added two scenes

---
+++ ThesisApp/Assets/GameController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+}
