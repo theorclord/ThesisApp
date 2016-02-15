3ae8c6f45f77398faebb789bf193672d9518575f
Mats Stenhaug <matsstenhaug@hotmail.com>
2016-02-15 14:47:15 +0100

    Node are now a gameobject, with scripts attached and made as prefab

--- ThesisApp/Assets/Scripts/NodeNode.cs
+++ ThesisApp/Assets/Scripts/NodeNode.cs
@@ -3,13 +3,52 @@ using System.Collections;
 
 public class NodeNode : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private string header = "Test";
+    private string flavour = "Test";
+    private GameObject selected;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
+
+    public void setHeader(string h)
+    {
+        this.header = h;
+    }
+
+    public void setFlavour(string f)
+    {
+        this.flavour = f;
+    }
+
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+               
+                    selected = hit.transform.gameObject;
+                    //Debug.Log(selected.tag);
+                    if (selected.tag == "LocationNode")
+                    {
+                    //Debug.Log("H: " + header + ", F:" + flavour);
+                        showLocationInfo(header, flavour);
+                    }
+                //selected.GetComponent<NodeNode>().
+
+            }
+        }
+        
+    }
+    private void showLocationInfo(string header, string flavour)
+    {
+        GameObject p = (GameObject.Find("Canvas").gameObject.transform.FindChild("EventPanel").gameObject);
+        p.GetComponent<EventPanel>().OpenWithText(header, flavour);
+       // p.SetActive(true);
+    }
 }
