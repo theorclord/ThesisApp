9e536f515193012b184b52aa243a368118604ea7
theorclord <mikkeltheorclord@hotmail.com>
2016-02-23 15:21:14 +0100

    minor shader test

---
+++ ThesisApp/Assets/Scripts/Test/TestCircle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class TestCircle : MonoBehaviour {
+
+    //private float circleSize = 1;
+    void Start()
+    {
+        /*
+        Handles.color = Color.red;
+        Handles.CircleCap(0, new Vector3(0, 0), Quaternion.identity, 2);
+        Handles.CircleCap(0, transform.position, transform.rotation, circleSize);
+        
+        Handles.color = Color.green;
+        Handles.CircleCap(0,
+                transform.position + Vector3(0, 5, 0),
+                transform.rotation,
+                circleSize);
+        Handles.color = Color.blue;
+        Handles.CircleCap(0,
+                transform.position + Vector3(0, 0, 5),
+                transform.rotation,
+                circleSize);
+        */
+    }
+	
+}
