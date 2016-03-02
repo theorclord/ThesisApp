42885037c46d4185d60f81dbc69c014d86155681
Mats Stenhaug <matsstenhaug@hotmail.com>
2016-03-02 13:28:30 +0100

    started working with the l-system

---
+++ ThesisApp/Assets/Scripts/PCG/L-System/LSystem.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LSystem : MonoBehaviour {
+
+    string axiom { get; set; }
+    public IDictionary ruleSets;
+    string expanded { get; set; }
+
+
+    public void expand(int depth)
+    {
+        string result = axiom;
+
+        // MORE Calculations needed for the nodes.
+        for(int i = 0; i < depth; i++)
+        {
+            char[] currDSA = result.ToCharArray();
+            string newString = "";
+            for(int j = 0; j < currDSA.Length; j++)
+            {
+                if (ruleSets.Contains(currDSA[j]))
+                {
+                    newString += ruleSets[currDSA[j]];
+                }
+                else
+                {
+                    newString += currDSA[j];
+                }
+            }
+            result = newString;
+        }
+        expanded = result;
+    }
+
+    public void interpret()
+    {
+        Stack stack = new Stack();
+        char[] chars = expanded.ToCharArray();
+        for (int i = 0; i < chars.Length; i++)
+        {
+            switch (chars[i])
+            {
+                case 'S':
+                    break;
+                case '[':
+                    stack.Push(new Object());
+                    break;
+                case ']':
+                    Object o = (Object)stack.Pop();
+                    break;
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ThesisApp/Assets/Scripts/PCG/L-System/State.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class State : MonoBehaviour {
+    double x;
+    double y;
+    double angle;
+    double length;
+    double turningAngle;
+
+    public State(double x, double y, double startAngle, double length, double turningA)
+    {
+        this.x = x;
+        this.y = y;
+        angle = startAngle;
+        this.length = length;
+        turningAngle = turningA;
+    }
+}
