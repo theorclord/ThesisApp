8e35bd536baa874254e27607d3d4526d7c0cf423
Mats Stenhaug <matsstenhaug@hotmail.com>
2016-04-13 12:11:57 +0200

    added more flavors, and added end credit flavor text

--- ThesisApp/Assets/Scripts/ScoreController.cs
+++ ThesisApp/Assets/Scripts/ScoreController.cs
@@ -1,13 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using System.Xml;
+using UnityEngine.UI;
 
 public class ScoreController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    private string eventstructurepath = "assets/scripts/XML/EventStructure.xml";
+    // Use this for initialization
+    void Start () {
+        XmlDocument xmlDoc = new XmlDocument();
+        xmlDoc.Load(eventstructurepath);
+        XmlNodeList list = xmlDoc.SelectNodes("eventstructure/endflavor/flavor");
+        int r = Random.Range(0, list.Count);
+        string endflavor = list[r].InnerText;
+        GameObject.FindGameObjectWithTag("MainCanvas").transform.FindChild("Panel").gameObject.transform.Find("Endtext").GetComponent<Text>().text = endflavor;
+    }
 	
 	// Update is called once per frame
 	void Update () {
