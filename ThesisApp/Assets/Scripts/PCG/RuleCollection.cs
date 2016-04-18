82cdc8fa190e99f772ce490af3fef606e1512ae4
Mats Stenhaug <matsstenhaug@hotmail.com>
2016-04-18 10:41:12 +0200

    more text and structure in eventstructure.xml

--- ThesisApp/Assets/Scripts/PCG/RuleCollection.cs
+++ ThesisApp/Assets/Scripts/PCG/RuleCollection.cs
@@ -22,16 +22,16 @@ public class RuleCollection{
         rule1.expandingIterations = 2;
         rule1.delta = Mathf.PI / 7.2f;
         ruleCollection.Add(rule1);
-
+        
         // Second Rule *STAR FORMATION*
         LRule rule2 = new LRule();
         rule2.name = "Star";
         rule2.axiom = "N--N--N";
         rule2.rules.Add("N", "N+N--N+N");
-        rule2.expandingIterations = 1;
+        rule2.expandingIterations = 3;
         rule2.delta = Mathf.PI / 3;
         ruleCollection.Add(rule2);
-
+        
         // Third Rule *Grass Straw*
         LRule rule3 = new LRule();
         rule3.name = "Grass Straw";
@@ -68,7 +68,7 @@ public class RuleCollection{
         rule6.expandingIterations = 2;
         rule6.delta = Mathf.PI / 2;
         ruleCollection.Add(rule6);
-
+        
     }
     
 }
